de73c84b8667049f369c9af1ccda88e1d7d9e444
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2024-01-05 20:01:32 -0600

    Added dark mode

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function toggleMode() {
+  var body = document.body;
+  var modeSwitch = document.getElementById("modeSwitch");
+  var icons = document.querySelectorAll(".icon");
+
+  if (modeSwitch.checked) {
+    body.classList.add("dark-mode");
+    icons.forEach(function (icon) {
+      var src = icon.getAttribute("src");
+      var newSrc = src.replace(".png", "-dm.png");
+      icon.setAttribute("src", newSrc);
+    });
+  } else {
+    body.classList.remove("dark-mode");
+    icons.forEach(function (icon) {
+      var src = icon.getAttribute("src");
+      var newSrc = src.replace("-dm.png", ".png");
+      icon.setAttribute("src", newSrc);
+    });
+  }
+}
